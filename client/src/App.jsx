051544b1b45a9434bc995a33c9c67c8acf3e6e25
bloch navaj <navajbloch420@gmail.com>
2024-07-20 21:03:48 +0530

    Add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,13 @@ import Project from "./Pages/Project";
 import Signin from "./Pages/Signin";
 import SignUp from "./Pages/SignUp";
 import Dashboard from "./Pages/Dashboard";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Navbar } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <>
+      <Navbar className="border-b-2">
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold "
+        >
+          <span className="px-2 bg-gradient-to-r from-indigo-500 via-purple-500 bg-pink-500 rounded-lg text-white">
+            Navaj
+          </span>
+          Blog
+        </Link>
+      </Navbar>
+    </>
+  );
+}
