a80370bdc5f24e2764f53289a798a6ee788195b7
bloch navaj <navajbloch420@gmail.com>
2024-07-22 10:08:53 +0530

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js'
+import autRoutes from './routes/auth.routes.js'
 
 dotenv.config();
 
@@ -17,10 +18,13 @@ mongoose.connect(
 
 const app = express();
 
+app.use(express.json());
+
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
 
 
-app.use('/api/user', userRoutes); 
+app.use('/api/user', userRoutes);
+app.use('/api/auth', autRoutes);
